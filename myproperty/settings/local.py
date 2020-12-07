4b698663b316d22b3bb358a0666c1e21b2009190
bimpro-edu <69068195+bimpro-edu@users.noreply.github.com>
2020-12-07 21:51:50 +0700

    initial commit

---
+++ myproperty/settings/local.py
@@ -0,0 +1,6 @@
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bimpro.edu@gmail.com'
+EMAIL_HOST_PASSWORD = 'pna3_PNA#_HAL'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
